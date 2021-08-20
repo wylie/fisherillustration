14cf0742cfa7c1a4fb7b94704fc5d61712de6e18
wylie <wylie@dukeofcheese.com>
2021-08-20 15:57:42 +0000

    Deploying to gh-pages from @ wylie/fisherillustration@fe0400336f97811e97c4570062ca4c12bf0cb99c 🚀

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Wrapper from './components/wrapper';
-
-import './_index.css';
-
-ReactDOM.render(<Wrapper />, document.getElementById('root'));
