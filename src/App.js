eff5d52338469c476b0f1728e01cb1f724c719d2
Wylie Fisher <wylie@dukeofcheese.com>
2023-08-07 19:29:52 +0000

    too many brackets

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const images = [
   { id: 1, image: require("./assets/images/18.jpg").default, thumbnail: require("./assets/images/18t.jpg").default },
   { id: 1, image: require("./assets/images/19.jpg").default, thumbnail: require("./assets/images/19t.jpg").default },
   { id: 20, image: require("./assets/images/20.jpg").default, thumbnail: require("./assets/images/20t.jpg").default },
-]];
+];
 
 const App = () => {
   const [activeImage, setActiveImage] = useState(1);
