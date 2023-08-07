e9fac2aeab79cf533b4aaf709bd5df2b53b3d3d3
Wylie Fisher <wylie@dukeofcheese.com>
2023-08-07 18:55:23 +0000

    why are we still having issues??

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
 import React, { useState } from "react";
+import {
+  Anchor,
+  Copy,
+  Wrap,
+  Sidebar,
+  Content,
+  Header,
+  Title,
+  Thumbs,
+  Image,
+  Nav,
+  Popup
+} from "./styled.js";
 
-import { Anchor, Copy, Wrap, Sidebar, Content, Header, Title, Thumbs, Image, Nav, Popup } from "./styled.js";
-
-// utility function to get all images and their thumbnails from a folder
+// utility function to get all images from a folder
 const importAll = (r) => {
   return r.keys().sort((a, b) => b > a).map(r);
 };
 
 const App = () => {
   const [activeImage, setActiveImage] = useState(1);
+  const thumbnailImages = importAll(require.context('./assets/images', false, /\.(png|jpe?g|svg)$/));
 
-  const handleClick = (evt) => {
+  const handleClick = evt => {
     const { target } = evt;
-    setActiveImage(Number(target.id));
+    setActiveImage(target.id);
   };
 
-  // require all images and thumbnails as an array, sorted by filename
-  const imagesAndThumbnails = importAll(require.context("./assets/images", false, /\.(png|jpe?g|svg)$/));
+  const renderThumbnails = () => {
+    return (
+      thumbnailImages.length > 0 &&
+      thumbnailImages.map((thumbnail) => (
+        <Image
+          key={thumbnail.id}
+          alt=""
+          onClick={handleClick}
+          src={thumbnail.thumbnail}
+          id={thumbnail.id}
+        />
+      ))
+    );
+  };
 
   return (
     <Wrap>
       <Sidebar>
         <Thumbs>
-          {imagesAndThumbnails.map(({ id, thumbnail }) => (
-            <Image
-              alt=""
-              onClick={handleClick}
-              src={thumbnail}
-              id={id}
-              key={id}
-            />
-          ))}
+          {renderThumbnails()}
         </Thumbs>
       </Sidebar>
       <Content>
@@ -39,7 +55,7 @@ const App = () => {
           <Copy />
           <Anchor href="mailto:mark@marksfisher.com" className="contact">
             <Nav>
-            <Popup>Get in touch with me by email at mark@marksfisher.com</Popup>
+              <Popup>Get in touch with me by email at mark@marksfisher.com</Popup>
             </Nav>
           </Anchor>
           <Anchor href="http://www.drawger.com/fisher/" className="drawger">
@@ -49,7 +65,7 @@ const App = () => {
           </Anchor>
         </Header>
         <Image
-          src={imagesAndThumbnails[activeImage - 1].image}
+          src={require(`./assets/images/${activeImage}.jpg`).default}
           alt=""
           className="bigImg"
         />
