75ba3dc49e3ffd43ae37610340e699490adbd675
Wylie Fisher <wylie@dukeofcheese.com>
2023-08-07 18:18:29 +0000

    revert these to the original from 2 years ago

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import {
-  Anchor,
+
+import { Anchor,
   Copy,
   Wrap,
   Sidebar,
@@ -10,64 +10,55 @@ import {
   Thumbs,
   Image,
   Nav,
-  Popup,
+  Popup
 } from "./styled.js";
 
-const importAll = (r) => r.keys().map(r);
+// utility function to get all images from a folder
+const importAll = (r) => {
+  return r.keys().sort((a,b) => b > a).map(r);
+}
+
+// require all thumbnails as an array, sorted by filename
+const thumbnailImages = importAll(require.context('./assets/tiny', false, /\.(png|jpe?g|svg)$/));
 
 const App = () => {
   const [activeImage, setActiveImage] = useState(1);
 
-  const handleClick = (evt) => {
+  // on click, look at the id attribute on the click target 
+  // and set that ID as the new active image
+  const handleClick = evt => {
     const { target } = evt;
     setActiveImage(target.id);
-  };
-
-  const thumbnails = importAll(require.context('./assets/tiny', false, /\.(png|jpe?g|svg)$/));
-
-  const renderThumbnails = () => {
-    return (
-      thumbnails.length > 0 &&
-      thumbnails.reverse().map((thumbnail, index) => (
-        <Image
-          key={index + 1}
-          alt=""
-          onClick={handleClick}
-          src={thumbnail.default} // Use the image path fetched by importAll
-          id={index + 1}
-        />
-      ))
-    );
-  };
+  }
 
   return (
     <Wrap>
-      <Sidebar>
-        <Thumbs>{renderThumbnails()}</Thumbs>
-      </Sidebar>
-      <Content>
-        <Header>
-          <Title />
-          <Copy />
-          <Anchor href="mailto:mark@marksfisher.com" className="contact">
-            <Nav>
+        <Sidebar>
+          <Thumbs>
+            {thumbnailImages.length && thumbnailImages.map((thumbnail, index) => ( // loop over array of thumbnails (note: images are numbered 1-21, instead of 0-20, so indexes are incremented by 1)
+              <Image alt="" onClick={handleClick} src={thumbnail.default} id={index + 1} key={index + 1} />
+            ))}
+          </Thumbs>
+        </Sidebar>
+        <Content>	
+          <Header>
+            <Title />
+            <Copy />
+            <Anchor href="mailto:mark@marksfisher.com" className="contact">
+              <Nav>
               <Popup>Get in touch with me by email at mark@marksfisher.com</Popup>
-            </Nav>
-          </Anchor>
-          <Anchor href="http://www.drawger.com/fisher/" className="drawger">
-            <Nav>
-              <Popup>A select group of illustrators showing and discussing their commercial and personal work. See a lot of my personal work.</Popup>
-            </Nav>
-          </Anchor>
-        </Header>
-        <Image
-          src={require(`./assets/large/${activeImage}.jpg`).default}
-          alt=""
-          className="bigImg"
-        />
-      </Content>
-    </Wrap>
-  );
+              </Nav>
+            </Anchor>
+            <Anchor href="http://www.drawger.com/fisher/" className="drawger">
+              <Nav>
+                <Popup>A select group of illustrators showing and discussing their commercial and personal work. See a lot of my personal work.</Popup>
+              </Nav>
+            </Anchor>
+          </Header>
+          <Image src={require(`./assets/large/${activeImage}.jpg`).default} alt="" className="bigImg" />
+        </Content>
+      </Wrap>
+  )
 };
 
 export default App;
