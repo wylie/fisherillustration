d4defd82171a45ee54c864e8c37bae5d4a3e465a
Wylie Fisher <wylie@dukeofcheese.com>
2023-08-07 18:34:59 +0000

    move the images into one folder and update App.js

--- src/App.js
+++ src/App.js
@@ -1,64 +1,57 @@
-import React, { useState } from "react";
-
-import { Anchor,
-  Copy,
-  Wrap,
-  Sidebar,
-  Content,
-  Header,
-  Title,
-  Thumbs,
-  Image,
-  Nav,
-  Popup
-} from "./styled.js";
-
-// utility function to get all images from a folder
+// utility function to get all images and their thumbnails from a folder
 const importAll = (r) => {
-  return r.keys().sort((a,b) => b > a).map(r);
-}
+  return r
+    .keys()
+    .sort((a, b) => {
+      const filenameA = a.split("/").pop();
+      const filenameB = b.split("/").pop();
+      return filenameA.localeCompare(filenameB);
+    })
+    .map((imagePath, index) => ({
+      id: index + 1,
+      image: r(imagePath).default,
+      thumbnail: r(imagePath.replace(/(\.\w+$)/, "t$1")).default,
+    }));
+};
 
-// require all thumbnails as an array, sorted by filename
-const thumbnailImages = importAll(require.context('./assets/tiny', false, /\.(png|jpe?g|svg)$/));
+// require all images and thumbnails as an array, sorted by filename
+const imagesAndThumbnails = importAll(
+  require.context("./assets/images", false, /\.(png|jpe?g|svg)$/)
+);
 
 const App = () => {
-  const [activeImage, setActiveImage] = useState(1);
+  // ...
 
-  // on click, look at the id attribute on the click target 
-  // and set that ID as the new active image
-  const handleClick = evt => {
+  const handleClick = (evt) => {
     const { target } = evt;
-    setActiveImage(target.id);
-  }
+    setActiveImage(Number(target.id));
+  };
 
   return (
     <Wrap>
-        <Sidebar>
-          <Thumbs>
-            {thumbnailImages.length && thumbnailImages.map((thumbnail, index) => ( // loop over array of thumbnails (note: images are numbered 1-21, instead of 0-20, so indexes are incremented by 1)
-              <Image alt="" onClick={handleClick} src={thumbnail.default} id={index + 1} key={index + 1} />
-            ))}
-          </Thumbs>
-        </Sidebar>
-        <Content>	
-          <Header>
-            <Title />
-            <Copy />
-            <Anchor href="mailto:mark@marksfisher.com" className="contact">
-              <Nav>
-              <Popup>Get in touch with me by email at mark@marksfisher.com</Popup>
-              </Nav>
-            </Anchor>
-            <Anchor href="http://www.drawger.com/fisher/" className="drawger">
-              <Nav>
-                <Popup>A select group of illustrators showing and discussing their commercial and personal work. See a lot of my personal work.</Popup>
-              </Nav>
-            </Anchor>
-          </Header>
-          <Image src={require(`./assets/large/${activeImage}.jpg`).default} alt="" className="bigImg" />
-        </Content>
-      </Wrap>
-  )
+      <Sidebar>
+        <Thumbs>
+          {imagesAndThumbnails.map(({ id, thumbnail }) => (
+            <Image
+              alt=""
+              onClick={handleClick}
+              src={thumbnail}
+              id={id}
+              key={id}
+            />
+          ))}
+        </Thumbs>
+      </Sidebar>
+      <Content>
+        {/* ... */}
+        <Image
+          src={imagesAndThumbnails[activeImage - 1].image}
+          alt=""
+          className="bigImg"
+        />
+      </Content>
+    </Wrap>
+  );
 };
 
 export default App;
